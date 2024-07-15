4ee735b2d00c68ed52790537da5dbcbae4d03154
Caique Menezes <caiquemenezes1@outlook.com>
2024-07-15 16:25:44 -0300

    criação do modal de cupom de desconto

--- src/components/modal_cart_itens/modal_cart_itens.js
+++ src/components/modal_cart_itens/modal_cart_itens.js
@@ -5,33 +5,55 @@ import './modal_cart_itens.css';
 import { Tooltip } from 'react-tooltip';
 
 
+
 const ModalCartItems = ({ cartItems, onClose }) => {
   return (
-    <div className="modal fade" id="modal_shoppingCart_id" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Carrinho de compras</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClose}></button>
+    <><div class="modal fade" id="modal_cupom_desconto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div className="modal-body">
-            {/* <-------Renderiza o componente ShoppingCart com os itens do carrinho -------> */}
-            <ShoppingCart items={cartItems} />
+          <div class="modal-body">
+            ...
           </div>
-          <div className="modal-footer">
-            <button type="button" className="btn-compra">Finalizar compra</button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-ticket-perforated-fill" viewBox="0 0 16 16" className='ticket-icons' data-tooltip-id="tooltip-cupom-desconto"
-                            data-tooltip-content="cumpom de desconto"
-                            data-tooltip-place="right"
-                            data-tooltip-offset= "5">
-                            
-  <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zm4-1v1h1v-1zm1 3v-1H4v1zm7 0v-1h-1v1zm-1-2h1v-1h-1zm-6 3H4v1h1zm7 1v-1h-1v1zm-7 1H4v1h1zm7 1v-1h-1v1zm-8 1v1h1v-1zm7 1h1v-1h-1z"/>
-</svg>
-<Tooltip id='tooltip-cupom-desconto'/>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
     </div>
+    
+    
+    <div className="modal fade" id="modal_shoppingCart_id" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Carrinho de compras</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClose}></button>
+            </div>
+            <div className="modal-body">
+              {/* <-------Renderiza o componente ShoppingCart com os itens do carrinho -------> */}
+              <ShoppingCart items={cartItems} />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn-compra">Finalizar compra</button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-ticket-perforated-fill" viewBox="0 0 16 16" className='ticket-icons' data-tooltip-id="tooltip-cupom-desconto"
+                data-tooltip-content="cumpom de desconto"
+                data-tooltip-place="right"
+                data-tooltip-offset="5"
+                data-bs-toggle="modal" data-bs-target="#modal_cupom_desconto"
+              >
+
+                <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zm4-1v1h1v-1zm1 3v-1H4v1zm7 0v-1h-1v1zm-1-2h1v-1h-1zm-6 3H4v1h1zm7 1v-1h-1v1zm-7 1H4v1h1zm7 1v-1h-1v1zm-8 1v1h1v-1zm7 1h1v-1h-1z" />
+              </svg>
+              <Tooltip id='tooltip-cupom-desconto' />
+            </div>
+          </div>
+        </div>
+      </div></>
   );
 };
 
