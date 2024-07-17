3c1028f7920788ef79f0503b4f019c8910fc92fd
Caique Menezes <caiquemenezes1@outlook.com>
2024-07-17 16:19:13 -0300

    atualização do botão de remover itens

--- src/components/header_component/header.js
+++ src/components/header_component/header.js
@@ -142,7 +142,7 @@ const Header_component = () =>{
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Cupom</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body" id='modal-body-cupom-desconto'>
