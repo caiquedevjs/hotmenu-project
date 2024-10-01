14055613ea4df33c5c15dd8133c1670f8c0abb3c
Caique Menezes <caiquemenezes1@outlook.com>
2024-10-01 13:57:23 -0300

    atualização do layout de perguntas do produto

--- src/components/modal_products_component/PerguntasComponent.js
+++ src/components/modal_products_component/PerguntasComponent.js
@@ -7,24 +7,20 @@ const PerguntasComponent = ({ productId }) => {
   console.log('Dados das perguntas:', additionalStates);
 
   return (
-    <div>
+    <div className="perguntas-container">
       {additionalStates.map(additional => (
-        <div key={additional.id} className='options-description-container'>
-          <div className='options-additions'>
-            <p>{additional.description}</p>
-          </div>
+        <div key={additional.id} className="pergunta-card">
+          <h3 className="pergunta-title">{additional.description}</h3>
 
           {/* Renderiza opções de observações como botões de rádio se for do tipo 1 */}
           {additional.type === 1 && additional.observacao.length > 0 && (
             <div className='observacoes'>
               {additional.observacao.map(obs => (
-                <label key={obs.Id}>
+                <label key={obs.Id} className="observacao-label">
                   <input 
                     type="radio" 
-                    name={`observacao_${additional.id}`} // Agrupa os botões de rádio
-                    onChange={() => {
-                      handleIncrement(obs.Id); // Adiciona a observação selecionada
-                    }} 
+                    name={`observacao_${additional.id}`} 
+                    onChange={() => handleIncrement(obs.Id)} 
                   />
                   {obs.Nome}
                 </label>
@@ -36,38 +32,42 @@ const PerguntasComponent = ({ productId }) => {
           {additional.options.length > 0 && (
             <div className='options-container'>
               {additional.options.map(option => (
-                <div className='options-icon'>
-                    <div className='option-text'> <p>{option.name} - R$ {option.price.toFixed(2).replace('.', ',')}</p></div>
-                
-                <div className='options-icons-plus-dash'>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-plus-circle-fill"
-                    viewBox="0 0 16 16"
-                    onClick={() => handleIncrement(option.id)}
-                    disabled={option.count === option.QtdMaximaPermitida}
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
-                  </svg>
-                  <span>{option.count}</span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-dash-circle-fill"
-                    viewBox="0 0 16 16"
-                    onClick={() => handleDecrement(option.id)}
-                    disabled={option.count === 0}
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
-                  </svg>
+                <div className='option-item' key={option.id}>
+                  <div className='option-info'>
+                    <p className="option-name">{option.name}</p>
+                    <p className="option-price">
+                      R$ {option.PrecoPromo ? option.PrecoPromo.toFixed(2).replace('.', ',') : option.price.toFixed(2).replace('.', ',')}
+                    </p>
+                  </div>
+
+                  <div className='options-icons'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-plus-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleIncrement(option.id)}
+                      disabled={option.count === option.QtdMaximaPermitida}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                    </svg>
+                    <span className="option-count">{option.count}</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-dash-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleDecrement(option.id)}
+                      disabled={option.count === 0}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                    </svg>
+                  </div>
                 </div>
-              </div>
-              
               ))}
             </div>
           )}
