a29fd5cc7d7140e949b4063eeaae8b9a92d5cabf
Caique Menezes <caiquemenezes1@outlook.com>
2024-10-08 14:05:34 -0300

    logica de validação de adicionais

--- src/components/modal_products_component/modal_products_component.js
+++ src/components/modal_products_component/modal_products_component.js
@@ -4,6 +4,7 @@ import useAdditionalState from './additionHandler';
 import { CartContext } from '../modal_cart_itens/CartContext';
 import { fetchEstabelecimentoData } from '../service/productService';
 import PerguntasComponent from './PerguntasComponent';
+import { Tooltip } from 'react-tooltip';
 
 const Modal_product_component = ({ id, product, onClose }) => {
   const { totalAdditional, additionalStates, handleIncrement, handleDecrement } = useAdditionalState(product.Id);
@@ -15,6 +16,7 @@ const Modal_product_component = ({ id, product, onClose }) => {
   const [error, setError] = useState(null);
   const [color, setColor] = useState("");
   const [suggestion, setSuggestion] = useState(""); // Novo estado para armazenar a sugestão
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   useEffect(() => {
     const fetchDataEstabelecimento = async () => {
@@ -77,16 +79,30 @@ const Modal_product_component = ({ id, product, onClose }) => {
     return totalPrice.toFixed(2);
 };
 
-
+const validateRequiredAdditions = () => {
+  const allRequiredMet = additionalStates.every(additional => {
+    if (additional.required) {
+      return additional.selectedCount >= additional.minOptions; // Verifica se ao menos a quantidade mínima foi selecionada
+    }
+    return true; // Para adicionais não obrigatórios, sempre retorna true
+  });
+  
+  setIsButtonDisabled(!allRequiredMet); // Desabilita o botão se não atender aos requisitos
+};
+useEffect(() => {
+  validateRequiredAdditions();
+}, [additionalStates]);
 
   const formatPrice = (price) => {
     return price.toFixed(2).replace('.', ',');
   };
 
   const handleAddToCart = () => {
-    const totalPrice = calculateTotalPrice();
-    addToCart(product, additionalStates, parseFloat(totalPrice), quantity, suggestion); // Envia a sugestão
-    onClose();
+    if (!isButtonDisabled) { // Adiciona ao carrinho apenas se o botão não estiver desabilitado
+      const totalPrice = calculateTotalPrice();
+      addToCart(product, additionalStates, parseFloat(totalPrice), quantity, suggestion);
+      onClose();
+    }
   };
 
   return (
@@ -106,7 +122,128 @@ const Modal_product_component = ({ id, product, onClose }) => {
 
            
 
-            <PerguntasComponent productId={product.Id} />
+            <div className="perguntas-container">
+      {additionalStates.map(additional => (
+        <div key={additional.id} className="pergunta-card">
+          <div className="pergunta-header">
+            <div className='pergunta-title-conteiner'>
+            <h3 className="pergunta-title">{additional.description}</h3>
+            <p className='max-option-text'>{`Escolha até ${additional.maxOptions} opções.`}</p>
+            </div>
+            
+            <span className="pergunta-info">
+              {additional.selectedCount} / {additional.maxOptions} 
+              {additional.required ? " (Obrigatório)" : " (Opcional)"}
+            </span>
+          </div>
+
+          {/* Renderiza opções de observações como botões de rádio se for do tipo 1 */}
+          {additional.type === 1 && additional.observacao.length > 0 && (
+            <div className='observacoes'>
+              {additional.observacao.map(obs => (
+                <label key={obs.Id} className="observacao-label">
+                  <input 
+                    type="radio" 
+                    name={`observacao_${additional.id}`} 
+                    onChange={() => handleIncrement(obs.Id)} 
+                  />
+                  {obs.Nome}
+                </label>
+              ))}
+            </div>
+          )}
+
+          {/* Renderiza opções de complemento */}
+          {additional.options.length > 0 && (
+            <div className='options-container'>
+              {additional.options.map(option => (
+                <div className='option-item' key={option.id}>
+                  <div className='option-info'>
+                    <p className="option-name">{option.name}</p>
+                    <p className="option-price">
+                    R$ {option.PrecoPromo ? option.PrecoPromo.toFixed(2).replace('.', ',') : option.price.toFixed(2).replace('.', ',')}
+                    </p>
+                  </div>
+
+                  <div className='options-icons'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-plus-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleIncrement(option.id)}
+                      disabled={option.count === option.QtdMaximaPermitida}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                    </svg>
+                    <span className="option-count">{option.count}</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-dash-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleDecrement(option.id)}
+                      disabled={option.count === 0}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                    </svg>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          
+          {/* Renderiza produtos com incremento e decremento */}
+          {additional.produtos && additional.produtos.length > 0 && (
+            <div className='options-container'>
+              {additional.produtos.map(produto => (
+                <div className='option-item' key={produto.Id}>
+                  <div className='option-info'>
+                    <p className="option-name">{produto.Nome}</p>
+                    <p className="option-price">
+                      R$ {produto.PrecoDeVenda.toFixed(2).replace('.', ',')}
+                    </p>
+                  </div>
+
+                  <div className='options-icons'>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-plus-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleIncrement(produto.Id, true)} // Passando true para indicar que é um produto
+                      disabled={produto.count === produto.QtdMaximaPermitida}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                    </svg>
+                    <span className="option-count">{produto.count || 0}</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-dash-circle-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => handleDecrement(produto.Id, true)} // Passando true para indicar que é um produto
+                      disabled={produto.count === 0}
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z"/>
+                    </svg>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
 
           </div>
           <div className='options-suggestion'>
@@ -132,7 +269,20 @@ const Modal_product_component = ({ id, product, onClose }) => {
               <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
             </svg><strong> Total:</strong> R$ {calculateTotalPrice()}</h5>
           </div>
-          <button className='options-btn-add' data-bs-dismiss="modal" aria-label="Close" style={{ backgroundColor : color}} onClick={handleAddToCart}>Adicionar ao carrinho</button>
+          <button className='options-btn-add' data-bs-dismiss="modal" aria-label="Close" 
+          style={{ backgroundColor : color}} 
+          onClick={handleAddToCart}  
+          disabled={isButtonDisabled}
+          data-tooltip-id="adicione-ao-carrinho-id"
+                  data-tooltip-content={
+                    isButtonDisabled
+                      ? "Selecione todas as opções obrigatórias"
+                      : "Adicionar ao carrinho"
+                  } 
+                  
+                  data-tooltip-place="top-start"
+          >Adicionar ao carrinho</button>
+           <Tooltip id='adicione-ao-carrinho-id'></Tooltip>
         </div>
       </div>
     </div>
