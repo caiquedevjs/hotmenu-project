64927add1015b21f4400c05cde51b6f2665ac320
caiquedevjs <caiquemenezes1@outlook.com>
2025-08-26 16:28:55 -0300

    resolvido bug de exclusão do valor do cupom

--- src/components/header_component/header.js
+++ src/components/header_component/header.js
@@ -639,6 +639,7 @@ const handleFinalizarPedido = async () => {
     setSelectedOption('');
     setValorTotalPedido("0,00");
     clearCart();
+    setDescontoAplicado()
 
   } catch (error) {
     console.error('Erro na chamada da API:', error);
@@ -655,8 +656,10 @@ const handleFinalizarPedido = async () => {
     }
     else{
     setList([])
-    setValorTotalPedido('"0,00"')
+    setValorTotalPedido("0.00")
     clearCart()
+    setDescontoAplicado()
+    
     
     
     toast.success("Pedido excluido com sucesso. 😞",  {theme: 'dark'})
@@ -885,7 +888,10 @@ const handleShow = () => setShow(true);
                   <div key={index} className="cart-item">
                     <div className='product-img-cart-item'>
                       <img src={`https://hotmenu.com.br/arquivos/${item.product.Foto}`} alt={item.product.Nome} className="cart-item-img" />
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16" id='remove-item-cart' onClick={() => removeFromCart(index)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16" id='remove-item-cart'onClick={() => {
+          removeFromCart(index);       // usa o index capturado pelo map
+          setDescontoAplicado();  // defina explicitamente o estado
+        }}>
                       <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                     </svg>
                       
