46104302f1f4fa3572677dc73643924b97ed298f
caiquedevjs <caiquemenezes1@outlook.com>
2025-10-02 17:55:33 -0300

    correção de bug

--- src/components/header_component/TextoTruncadoComModal.js
+++ src/components/header_component/TextoTruncadoComModal.js
@@ -18,7 +18,7 @@ const TextoTruncadoComModal = ({ texto, maxCaracteres = 120, color }) => {
     <>
       {/* O texto truncado com o botão para abrir o modal */}
       <h6 className='estabelecimento-description'>
-        {textoTruncado}
+        
         <button onClick={() => setModalAberto(true)} className="ver-mais-btn">
           Ver mais
         </button>
@@ -44,7 +44,7 @@ const TextoTruncadoComModal = ({ texto, maxCaracteres = 120, color }) => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-info-circle-fill" viewBox="0 0 16 16" style={{ color: color || '#0d6efd' }}>
                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
                     </svg>
-                    Descrição Completa
+                    Descrição 
                   </h3>
                   <button type="button" className="btn-close" onClick={() => setModalAberto(false)} aria-label="Close"></button>
                 </div>
